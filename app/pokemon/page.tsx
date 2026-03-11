8eb529487fabd22456933103223bbb77a43d5178
奥村将太 <ShotaOkumura@Shota-MacBook-Pro-2.local>
2026-03-11 15:30:27 +0900

    added pagenation

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import PokemonList from "./components/PokemonList/PokemonList";
+import { M_PLUS_1 } from "next/font/google";
 
 type Pokemon = {
   name: string;
@@ -10,8 +11,12 @@ type Pokemon = {
 // ひとつ一つの要素の型
 
 type PokemonResponse = {
+  count: number;
   results: Pokemon[];
 };
+
+
+
 // 返ってくるデータの型
 type PokemonDetail = {
   name: string;
@@ -21,33 +26,62 @@ type PokemonDetail = {
 };
 // typesの配列は[{ type: { name: "grass" } },{ type: { name: "poison" } }]のような形。
 
+
+const PAGE_WINDOW = 5;
+
 export default function Page() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const [selected, setSelected] = useState<PokemonDetail | null>(null);
+  const [page, setPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<PokemonDetail | null>(null);
   //   const [selected, setSelected]=useState(null);だと型推論でsetStateもnullになるので、このstateで使う型をジェネリクスで書く必要がある
   //   状態で変化するものは何か→変化する状態をstateで管理。errorをstate管理するのはエラーが出たときに画面に表示することを可能にするため
-  const [detailLoading, setDetailLoading] = useState(false);
-  const [offset, setOffset] = useState(0);
 
-  async function load() {
+  const [loading, setLoading] = useState(false);
+  const [detailLoading, setDetailLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [pokemonPerPage, setPokemonPerPage] = useState(10);
+  const offset = (page - 1) * pokemonPerPage;
+  const totalPages = Math.ceil(totalCount / pokemonPerPage); //切り上げ
+  const HALF_WINDOW = Math.floor(PAGE_WINDOW / 2); //切り捨て
+
+  let startPage = page - HALF_WINDOW;
+  let endPage = page + HALF_WINDOW;
+  if (startPage < 1) {
+    startPage = 1;
+    endPage = PAGE_WINDOW;
+  }
+  if (endPage > totalPages) {
+    endPage = totalPages;
+    startPage = totalPages - PAGE_WINDOW + 1;
+  }
+
+  const visiblePages: number[] = [];
+  for (let i = startPage; i <= endPage; i++) {
+    visiblePages.push(i);
+  }
+
+
+  const load = async () => {
     setLoading(true);
+    setError(null); // ← あると便利（前のエラーを消す）
     try {
-      setError(null); // ← あると便利（前のエラーを消す）
       const res = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`,
+        `https://pokeapi.co/api/v2/pokemon?limit=${pokemonPerPage}&offset=${offset}`,
       );
       if (!res.ok) {
         throw new Error(`HTTP ${res.status}`);
       }
       //   Errorクラスの中のmessageプロパティに`HTTP ${res.status}`という文字列が入る。
+      //   HTTP通信自体がうまくいっている場合、catchにはいらない。Fetcherror≠HTTPerror
       //   okはresponseオブジェクトのプロパティ名でboolean型
       //   ?limit=10はクエリパラメータ?key=valueの形で使う。
       const data: PokemonResponse = await res.json();
       // ここで返ってきているのはcount: number;　next: string | null; results: Pokemon[];などのオブジェクト。
       // その中のresultsがPokemon[]なのでPokemonResponseという型を定義している。（余分なプロパティがあってその中の一つの型を指定している。逆にプロパティがその型に対して足りないのはNG）
-      setPokemon(data.results); //ここで初めて画面が変わる。
+      setPokemonList(data.results); //ここで初めて画面が変わる。
+      setTotalCount(data.count);
     } catch (e) {
       setError(e instanceof Error ? e.message : "Unknown Error");
     } //instanceof は JSの型チェックの仕組み。今回のerrorが Errorクラスから作られたオブジェクトかどうかを調べてる。errorをUIで表示させるためのuseState
@@ -57,7 +91,7 @@ export default function Page() {
     // tryで囲むのは一連の非同期処理をまとめて囲む
   }
 
-  async function loadDetail(url: string) {
+  const loadDetail = async (url: string) => {
     setDetailLoading(true);
     try {
       const res = await fetch(url);
@@ -65,8 +99,7 @@ export default function Page() {
         throw new Error(`HTTP ${res.status}`);
       }
       const data: PokemonDetail = await res.json();
-      console.log(data);
-      setSelected(data);
+      setSelectedPokemon(data);
     } catch (e) {
       setError(e instanceof Error ? e.message : "Unknown Error");
     }
@@ -75,37 +108,84 @@ export default function Page() {
 
   useEffect(() => {
     load();
-  }, [offset]);
-  // offsetが変わるたび自動で。
+  }, [page]);
+  // pageが変わるたび自動で。
 
   return (
     <main style={{ padding: 20 }}>
       <h1>Pokemon List</h1>
-      <button onClick={load} disabled={loading}>
-        {/* loadingがtrueの時はbuttonを押せないようにしているdisabledは真偽型の属性。 */}
-        Reload 10 pokemon
-      </button>
       <br />
-      <button onClick={() => setOffset((prev) => Math.max(0, prev - 10))}>
-        Prev
-      </button>
-      <br /> {/*  Math.max(a,b)は大きい方を返すという意味 */}
-      <button onClick={() => setOffset((prev) => prev + 10)}>Next</button>
       {error && <p style={{ color: "red" }}>{error}</p>}
       {/* このerrorはuseStateのerror、つまりstringかnull*/}
+
       <PokemonList
-        pokemon={pokemon}
+        pokemon={pokemonList}
         detailLoading={detailLoading}
         onSelect={loadDetail}
       />
+       <button onClick={() => setPage((prev) => prev - 1)} disabled={page === 1}>
+        Prev
+      </button>
+      {startPage > 1 && (
+        <>
+          <button onClick={() => setPage(1)} style={{ margin: "0 4px" }}>
+            1
+          </button>
+
+          {startPage > 2 && <span style={{ margin: "0 4px" }}>...</span>}
+        </>
+      )}
+
+      {visiblePages.map((p) => (
+        <button
+          key={p}
+          onClick={() => setPage(p)}
+          style={{
+            margin: "0 4px",
+          }}
+        >
+          {p}
+        </button>
+      ))}
+
+          {endPage < totalPages && (
+        <>
+          {endPage < totalPages - 1 && <span style={{ margin: "0 4px" }}>...</span>}
+          <button
+            onClick={() => setPage(totalPages)}
+            style={{ margin: "0 4px" }}
+          >
+            {totalPages}
+          </button>
+        </>
+      )}
+      <button onClick={() => setPage((prev) => prev + 1)}>
+        Next
+      </button>
       <hr />
+      <label>
+  表示件数:
+  <select
+    value={pokemonPerPage}
+    onChange={(e) => {
+      setPokemonPerPage(Number(e.target.value));
+      setPage(1);
+    }}
+  >
+    {[10, 30, 50].map((n) => (
+      <option key={n} value={n}>
+        {n}
+      </option>
+    ))}
+  </select>
+</label>
       {detailLoading && <p>Loading detail...</p>}
       <h2>Detail</h2>
-      {selected ? (
+      {selectedPokemon ? (
         <div>
-          <p>Name:{selected.name}</p> <p>Height:{selected.height}</p>
-          <p>Weight:{selected.weight}</p>
-          <p>Types:{selected.types.map((t) => t.type.name).join(",")}</p>
+          <p>Name:{selectedPokemon.name}</p> <p>Height:{selectedPokemon.height}</p>
+          <p>Weight:{selectedPokemon.weight}</p>
+          <p>Types:{selectedPokemon.types.map((t) => t.type.name).join(",")}</p>
           {/* Joinで配列["grass", "poison"]→"grass,poison"（文字列）という作業 */}
           {/* 三項演算子を使った条件レンダリング */}
         </div>
